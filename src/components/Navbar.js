2cf15f11fee53fc01ed67c37fe781113c74d193a
Christian Castillo <castillochristian3@gmail.com>
2023-09-07 12:06:22 -0400

    created Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-800'>
+
+    </div>
+  )
+}
+
+export default Navbar
